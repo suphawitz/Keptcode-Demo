eae1a8a97c53b26a011ec4baa1f76f3992951c4f
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-05-02 23:47:17 +0700

    assets and add framwork

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// // ===============================
+// // ------ Responsive Navbar ------
+// // ===============================
+
+// // Toggle to show and hide navbar menu
+// let navbarMenu = document.getElementById("menu");
+// let navbarMenuCon = document.getElementById("menu-contact");
+// let burgerMenu = document.getElementById("burger");
+
+// burgerMenu.addEventListener("click", () => {
+//   navbarMenu.classList.toggle("dp-nav-active");
+// //   navbarMenuCon.classList.toggle("dp-nav-active");
+//   burgerMenu.classList.toggle("dp-nav-active");
+// });
