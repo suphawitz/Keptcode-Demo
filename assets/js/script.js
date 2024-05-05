2dc436a313d4c2c328db6dcfc8556552ba1b952a
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-05-05 22:47:27 +0700

    Section 01 | Online Document

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,17 @@
 // //   navbarMenuCon.classList.toggle("dp-nav-active");
 //   burgerMenu.classList.toggle("dp-nav-active");
 // });
+
+// Nav menu if scroll is change background
+window.addEventListener('scroll', () => {
+    const verticalScrollPx = window.scrollY || window.pageYOffset;
+    const navMenu = document.getElementById("navmenu");
+  
+    // console.log(verticalScrollPx > 500);
+    if (verticalScrollPx > 10) {
+      navMenu.classList.add("kc-nav-bgchange");
+      // document.body.style.backgroundColor = 'red';
+    } else {
+      navMenu.classList.remove("kc-nav-bgchange");
+    }
+  });
