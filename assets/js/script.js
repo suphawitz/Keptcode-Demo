6db71039b69d4a59450adca6fa2cbf8aa9667509
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-05-12 12:28:06 +0700

    Section 05

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,43 @@ window.addEventListener('scroll', () => {
     } else {
       navMenu.classList.remove("kc-nav-bgchange");
     }
+});
+
+
+// SlickJS 
+$(document).ready(function(){
+
+  // Slider Section 05 
+  $('.kc-sc05-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1084,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 774,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
   });
+
+  // $('.es-gallery-slider .slick-prev').html('<i class="fa-solid fa-angle-left"></i>');
+  // $('.es-gallery-slider .slick-next').html('<i class="fa-solid fa-angle-right"></i>');
+
+});
